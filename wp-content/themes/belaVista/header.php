4db14c51aff8a03c683fa71a1f6782c91c1c8427
agnaldo <agnaldobernardojunior@yahoo.com.br>
2015-12-30 01:04:27 -0200

    ajuste jquery

--- wp-content/themes/belaVista/header.php
+++ wp-content/themes/belaVista/header.php
@@ -15,8 +15,10 @@
     <script src="<?php echo get_template_directory_uri(); ?>/inc/scripts/selectivizr-min.js" type="text/javascript"></script>
 <![endif]-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="stylesheet"
 	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	
 <script
 	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
@@ -24,7 +26,7 @@
 	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 
-<script src="js/vendors/jquery/jquery.min.js"></script>
+
 <script
 	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   
