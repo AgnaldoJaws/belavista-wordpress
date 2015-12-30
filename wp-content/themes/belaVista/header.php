dafb7e3557d8f6c2427c6e1e52a51b40a711140e
agnaldo <agnaldobernardojunior@yahoo.com.br>
2015-12-30 01:44:24 -0200

    bootstrap

--- wp-content/themes/belaVista/header.php
+++ wp-content/themes/belaVista/header.php
@@ -16,7 +16,20 @@
 <![endif]-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+
+
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  
 <?php wp_head(); ?>
 
 <style>
